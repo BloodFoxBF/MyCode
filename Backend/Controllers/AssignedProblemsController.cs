4af846591da74b7b390e79f1d37b543ce5f3e685
nicemanman\Илья <ilya.falko2013@gmail.com>
2020-10-31 20:08:39 +0300

    Изменения в части Frontend

---
+++ Backend/Controllers/AssignedProblemsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Backend.Models;
+using Backend.Extensions;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssignedProblemsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AssignedProblemsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/AssignedProblems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AssignedProblemDTO>>> GetAssignedTasks(int studentID, bool isSolved, DateTime startDate, DateTime finishDate)
+        {
+            var assignedProblemList = await _context.GetAllAsync<AssignedProblem>(x => x.StudentId == studentID && x.isSolved == isSolved && (x.StartDate >= startDate && x.StartDate < finishDate));
+            List<AssignedProblemDTO> assignedProblemDTOs = new List<AssignedProblemDTO>();
+            foreach (var assignedProblem in assignedProblemList)
+            {
+                var assignedProblemDTO = new AssignedProblemDTO()
+                {
+                    StudentId = assignedProblem.StudentId,
+                    ProblemId = assignedProblem.ProblemId,
+                    Mark = assignedProblem.Mark
+                };
+                assignedProblemDTOs.Add(assignedProblemDTO);
+            }
+
+            return new ActionResult<IEnumerable<AssignedProblemDTO>>(assignedProblemDTOs);
+        }
+
+        // GET: api/AssignedProblems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AssignedProblem>> GetAssignedProblem(int id)
+        {
+            var assignedProblem = await _context.AssignedTasks.FindAsync(id);
+
+            if (assignedProblem == null)
+            {
+                return NotFound();
+            }
+
+            return assignedProblem;
+        }
+
+        // PUT: api/AssignedProblems/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAssignedProblem(int id, AssignedProblem assignedProblem)
+        {
+            if (id != assignedProblem.AssignedProblemId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(assignedProblem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AssignedProblemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/AssignedProblems
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<AssignedProblem>> PostAssignedProblem(AssignedProblem assignedProblem)
+        {
+            _context.AssignedTasks.Add(assignedProblem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAssignedProblem", new { id = assignedProblem.AssignedProblemId }, assignedProblem);
+        }
+
+        // DELETE: api/AssignedProblems/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<AssignedProblem>> DeleteAssignedProblem(int id)
+        {
+            var assignedProblem = await _context.AssignedTasks.FindAsync(id);
+            if (assignedProblem == null)
+            {
+                return NotFound();
+            }
+
+            _context.AssignedTasks.Remove(assignedProblem);
+            await _context.SaveChangesAsync();
+
+            return assignedProblem;
+        }
+
+        private bool AssignedProblemExists(int id)
+        {
+            return _context.AssignedTasks.Any(e => e.AssignedProblemId == id);
+        }
+    }
+}
---
+++ Backend/Migrations/20201031123801_addTests.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Backend.Migrations
+{
+    public partial class addTests : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Problems",
+                columns: table => new
+                {
+                    ProblemId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Legend = table.Column<string>(nullable: true),
+                    Requirement = table.Column<string>(nullable: true),
+                    Example = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Problems", x => x.ProblemId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    TeacherId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.TeacherId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Test",
+                columns: table => new
+                {
+                    TestId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(nullable: true),
+                    ProblemId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Test", x => x.TestId);
+                    table.ForeignKey(
+                        name: "FK_Test_Problems_ProblemId",
+                        column: x => x.ProblemId,
+                        principalTable: "Problems",
+                        principalColumn: "ProblemId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    GroupId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    TeacherId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.GroupId);
+                    table.ForeignKey(
+                        name: "FK_Groups_Teachers_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "Teachers",
+                        principalColumn: "TeacherId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    GroupId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                    table.ForeignKey(
+                        name: "FK_Students_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "GroupId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AssignedTasks",
+                columns: table => new
+                {
+                    AssignedProblemId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StudentId = table.Column<int>(nullable: false),
+                    ProblemId = table.Column<int>(nullable: false),
+                    Mark = table.Column<int>(nullable: false),
+                    isSolved = table.Column<bool>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssignedTasks", x => x.AssignedProblemId);
+                    table.ForeignKey(
+                        name: "FK_AssignedTasks_Problems_ProblemId",
+                        column: x => x.ProblemId,
+                        principalTable: "Problems",
+                        principalColumn: "ProblemId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AssignedTasks_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssignedTasks_ProblemId",
+                table: "AssignedTasks",
+                column: "ProblemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssignedTasks_StudentId",
+                table: "AssignedTasks",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_TeacherId",
+                table: "Groups",
+                column: "TeacherId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_GroupId",
+                table: "Students",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Test_ProblemId",
+                table: "Test",
+                column: "ProblemId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AssignedTasks");
+
+            migrationBuilder.DropTable(
+                name: "Test");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Problems");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.DropTable(
+                name: "Teachers");
+        }
+    }
+}
